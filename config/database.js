59074619af6c6305f084d6acdb01ae880b15af80
Korede Victor Adebayo <korvictech@gmail.com>
2019-08-14 22:53:03 +0100

    Mongodb cloud set-up

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports ={
-    database : 'mongodb://localhost:27017/supplierpaydb',
+    database: 'mongodb+srv://korvictech:withCHRIST@cluster0-4jsde.mongodb.net/test?retryWrites=true&w=majority',
+    // database : 'mongodb://localhost:27017/supplierpaydb',
     secret: 'secretPassword',
     paySecret: 'sk_test_6f712e12209429fb6fb9985b1bf1357e65795802'
 }
